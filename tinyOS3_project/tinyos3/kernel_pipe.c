6f87704ec2ae7b89845a08e9905d5e0c691a20b6
markvgns <133893812+markvgns@users.noreply.github.com>
2024-12-01 13:18:58 +0200

    ohhhh
    ahhhh

--- tinyOS3_project/tinyos3/kernel_pipe.c
+++ tinyOS3_project/tinyos3/kernel_pipe.c
@@ -4,75 +4,92 @@
 #include "kernel_streams.h"
 #include "kernel_dev.h"
 
-int sys_Pipe(pipe_t *pipe)
+int sys_Pipe(pipe_t* pipe)
 {
-	pipe_cb *Pipe_cb = (pipe_cb *)xmalloc(sizeof(pipe_cb));
+    pipe_cb *Pipe_cb = (pipe_cb *)xmalloc(sizeof(pipe_cb));
 
-	if (FCB_reserve(1, &pipe->read, &CURPROC->FIDT[MAX_FILEID]) &&
-			FCB_reserve(1, &pipe->write, &CURPROC->FIDT[MAX_FILEID]) != 0)
-	{
 
-		Pipe_cb->reader = pipe->read;
-		Pipe_cb->writer = pipe->write;
+    if(FCB_reserve(1,&pipe->read, &CURPROC->FIDT[MAX_FILEID]) && 
+    FCB_reserve(1,&pipe->write, &CURPROC->FIDT[MAX_FILEID]) != 0){
 
-		Pipe_cb->has_space = COND_INIT; /* For blocking writer if no space is available */
+        Pipe_cb->reader = CURPROC->FIDT[pipe->read];
+        Pipe_cb->writer = CURPROC->FIDT[pipe->write];
 
-		Pipe_cb->has_data = COND_INIT; /* For blocking reader until data are available */
+	      Pipe_cb->has_space = COND_INIT;    /* For blocking writer if no space is available */
 
-		Pipe_cb->w_position = 0; /* write, read position in buffer (it depends on your implementation of bounded buffer, i.e. alternatively pointers can be used) */
+        Pipe_cb->has_data = COND_INIT;     /* For blocking reader until data are available */
 
-		Pipe_cb->r_position = 0;
+	      Pipe_cb->w_position = 0;  /* write, read position in buffer (it depends on your implementation of bounded buffer, i.e. alternatively pointers can be used) */
 
-		Pipe_cb->BUFFER[PIPE_BUFFER_SIZE];
+        Pipe_cb->r_position = 0;
 
-		Pipe_cb->reader->streamobj = Pipe_cb;
+		    Pipe_cb->BUFFER[PIPE_BUFFER_SIZE]; 
 
-		Pipe_cb->writer->streamobj = Pipe_cb;
+        Pipe_cb->reader->streamobj = Pipe_cb;
 
-		Pipe_cb->reader->streamfunc = &reader_file_ops;
+        Pipe_cb->writer->streamobj = Pipe_cb;
 
-		Pipe_cb->writer->streamfunc = &writer_file_ops;
+        Pipe_cb->reader->streamfunc = &reader_file_ops;
 
-		return 0;
-	}
-	else
-	{
-		return -1;
-	}
+        Pipe_cb->writer->streamfunc = &writer_file_ops;
+
+
+        return 0;
+
+	}else
+    {return -1;}
+
+
 }
 
-int pipe_write(void *pipecb_t, const char *buf, unsigned int n)
+int pipe_write(void* pipecb_t, const char *buf, unsigned int n)
 {
+    return 0; 
+
 }
 
-int pipe_read(void *pipecb_t, char *buf, unsigned int n)
+int pipe_read(void* pipecb_t, char *buf, unsigned int n)
 {
+    return 0; 
+
 }
 
-int pipe_writer_close(void *_pipecb)
+
+int pipe_writer_close(void* _pipecb)
 {
+    return 0; 
+
 }
 
-int pipe_reader_close(void *_pipecb)
+
+int pipe_reader_close(void* _pipecb)
 {
+    return 0; 
+
 }
 
-int returnError(void *dev, const char *buf, unsigned int size)
+int returnError_write(void* dev, const char* buf, unsigned int size)
 {
-	return -1;
+    return -1;
 }
 
-/** @brief reader and writer file ops structures
- */
+int returnError_read(void* dev, char* buf, unsigned int size)
+{
+    return -1;
+}
 
-static file_ops reader_file_ops = {
-		.Open = NULL,
-		.Read = pipe_read,
-		.Write = returnError,
-		.Close = pipe_reader_close};
 
-static file_ops writer_file_ops = {
-		.Open = NULL,
-		.Read = returnError,
-		.Write = pipe_write,
-		.Close = pipe_writer_close};
+file_ops reader_file_ops = {
+  .Open = NULL,
+  .Read = pipe_read,
+  .Write = returnError_write,
+  .Close = pipe_reader_close
+};
+
+file_ops writer_file_ops = {
+  .Open = NULL,
+  .Read = returnError_read,
+  .Write = pipe_write,
+  .Close = pipe_writer_close
+};
+
--- tinyOS3_project/tinyos3/kernel_pipe.h
+++ tinyOS3_project/tinyos3/kernel_pipe.h
@@ -1,31 +1,42 @@
+#ifndef __KERNEL_PIPE_H
+#define __KERNEL_PIPE_H
+
+
+
 #include "util.h"
 #include "tinyos.h"
 #include "kernel_sched.h"
 #include "kernel_threads.h"
+#include "kernel_dev.h"
 
 #define PIPE_BUFFER_SIZE 20
 
-typedef struct pipe_control_block
-{
-  FCB *reader, *writer;
-  CondVar has_space;             /* For blocking writer if no space is available */
-  CondVar has_data;              /* For blocking reader until data are available */
-  int w_position, r_position;    /* write, read position in buffer (it depends on your implementation of bounded buffer, i.e. alternatively pointers can be used)*/
-  char BUFFER[PIPE_BUFFER_SIZE]; /* bounded (cyclic) byte buffer */
+typedef struct pipe_control_block {
+	FCB *reader, *writer;
+	CondVar has_space;    /* For blocking writer if no space is available */
+    CondVar has_data;     /* For blocking reader until data are available */
+	int w_position, r_position;  /* write, read position in buffer (it depends on your implementation of bounded buffer, i.e. alternatively pointers can be used)*/
+	char BUFFER[PIPE_BUFFER_SIZE];   /* bounded (cyclic) byte buffer */
 } pipe_cb;
 
-/**
-  @brief returns -1
-*/
+extern file_ops reader_file_ops;
+extern file_ops writer_file_ops;
 
-int returnError(void *dev, const char *buf, unsigned int size);
+int returnError_write(void* dev, const char* buf, unsigned int size);
 
-int sys_Pipe(pipe_t *pipe);
+int returnError_read(void* dev, char* buf, unsigned int size);
 
-int pipe_write(void *pipecb_t, const char *buf, unsigned int n);
+int sys_Pipe(pipe_t* pipe);
 
-int pipe_read(void *pipecb_t, char *buf, unsigned int n);
+int pipe_write(void* pipecb_t, const char *buf, unsigned int n);
 
-int pipe_writer_close(void *_pipecb);
+int pipe_read(void* pipecb_t, char *buf, unsigned int n);
 
-int pipe_reader_close(void *_pipecb);
+int pipe_writer_close(void* _pipecb);
+
+
+int pipe_reader_close(void* _pipecb);
+
+
+#endif
+
